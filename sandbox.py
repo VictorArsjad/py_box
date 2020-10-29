5ce2f8e966632f5cb00244889076e82577253b5b
Victor Arsjad <victor.arsjad@mokapos.com>
2020-10-29 21:47:26 +0700

    completed tutorial python java

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,5 @@
+from pip._vendor.distlib.compat import raw_input
+
 from clock import ping
 
 # cheatsheet
@@ -11,6 +13,7 @@ print("\n2. Testing out if statements")
 value = 0
 if 0 == value:
     print("Number is zero!")
+# elif ... :
 else:
     print("Message is not zero!")
 
@@ -57,4 +60,64 @@ print("current sample list after appending the number 2: %s" % sample_list)
 print("list created using = is %s" % list2)
 print("list created using clone is %s" % list3)
 
+print("\n7. Tuples")
+tuple1 = (1, 2, 'Sprite')
+print("tuple index 1 is %s" % tuple1[1])
+print("tuple index 2 is %s" % tuple1[2])
 
+print("\n8. Dictionary")
+print("it does have to be Key: String, and Value: number! It can be anything.")
+room_num = {'john': 121, 'tom': 307}
+guest_names = ["john", "tom"]
+
+for name in guest_names:
+    print("%s's room is %s" % (name, room_num[name]))
+
+print("\n9. Operators")
+print("20 ** 2 is %s" % 20 ** 2)
+print("20 %% 2 is %s" % (20 % 2))
+
+print("\n10. For Loops")
+for x in range(0, 3):
+    print("This is loop %s!" % x)
+
+for y in range(0, 6, 2):
+    print("Increment this by 2! = %s" % y)
+
+print("Launching in:")
+for y in range(3, 0, -1):
+    print(y)
+print("Lift off!")
+
+print("\n11. While Loops")
+var1 = 3
+while var1 < 20:
+    var1 = var1 * 2
+    print(var1)
+print("Exit loop.")
+
+while True:
+    n = raw_input("Please enter `hello`: ")
+    if n.strip() == 'hello':
+        break
+    else:
+        print("NO DAS NOT HELLO! ")
+
+print("\n12. Iterations")
+print("iteration can be done on lists, tuples, and dictionaries")
+friends = ['Among', 'Akiao', 'Aleng']
+for friend in friends:
+    print(friend)
+
+tup = ('alpha', "bravo", "charlie")
+for val in tup:
+    print(val)
+
+codes = {'INDIA': 'in',
+         'USA': 'us',
+         'UK': 'gb'}
+for key in codes:
+    print(key, 'corresponds to', codes[key])
+
+for key, value in codes.items():  # Python 3
+    print(key, 'corresponds to', codes[key])
